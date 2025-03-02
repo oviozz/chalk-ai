64cbbe41560cf7bed3a901bb77ef6d95b481c7cc
bazalu <bazaluu@gmail.com>
2025-03-02 01:35:17 -0800

    landig

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
+
 "use client";
 import { useState } from "react";
-import Whiteboard from "@/components/whiteboard";
 import LandingPage from "@/components/Landingpage";
-import TopicSelector from "@/components/topic-selector";
 
 export type TopicType = {
     topicID: string,
@@ -10,42 +9,11 @@ export type TopicType = {
 }
 
 export default function Home() {
-    // Authentication state
     const [isLoggedIn, setIsLoggedIn] = useState(false);
-    
-    // Topic selection state
-    const [showTopicSelector, setShowTopicSelector] = useState(true);
-    const [selectedTopic, setSelectedTopic] = useState<TopicType | null>(null);
-
-    const clearTopic = () => {
-        setSelectedTopic(null);
-        setShowTopicSelector(true);
-    }
-
-    const markdownText = ``
 
     return (
         <div>
-            {!isLoggedIn ? (
-                <LandingPage onLogin={() => setIsLoggedIn(true)} />
-            ) : (
-                <>
-                    {(!selectedTopic?.topicID || !selectedTopic?.prompt) && (
-                        <TopicSelector
-                            onClose={() => setShowTopicSelector(false)}
-                            onSelectTopic={(topicID: string, prompt: string) => setSelectedTopic({
-                                topicID,
-                                prompt
-                            })}
-                        />
-                    )}
-                    <Whiteboard
-                        clearTopic={clearTopic}
-                        topic={selectedTopic}
-                        problem_generated={markdownText}
-                    />
-                </>
-            )}
+            <LandingPage onLogin={() => setIsLoggedIn(true)} />
         </div>
     );
 }
